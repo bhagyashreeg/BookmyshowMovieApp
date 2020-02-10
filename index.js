7e1a26a1805894d15c2cf46cc144197d81ea9f2d
prajwalkumar971 <prajwalkumar971@gmail.com>
2020-02-10 17:29:13 +0530

    added JS

---
+++ index.js
@@ -0,0 +1,52 @@
+let originalMoviesList = [
+    {
+        name: "War Horse",
+        url: "images/War_Horse.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "LaughTon ohara",
+        url: "images/LaughTon_ ohara.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "Red Cliff",
+        url: "images/Red_Cliff.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "How to train your dragon ",
+        url: "images/How_to_train_your_dragon_3_4.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "Mr Bean",
+        url: "images/Mr_Bean.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "Family",
+        url: "images/Family.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "Panda",
+        url: "images/Panda.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "Frozen",
+        url: "images/Frozen.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "KGF",
+        url: "images/KGF.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    },
+    {
+        name: "HouseFull 4", 
+        url: "images/Housefull.jpg",
+        tag: "lepsum lepsum lepsum lepsum"
+    }
+];
